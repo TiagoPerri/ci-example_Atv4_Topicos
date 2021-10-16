3e5401fd7f5f869e5b2505dbfb48865aaeb23e76
TiagoPerri <tiagocnsr@gmail.com>
2021-10-16 01:17:36 -0300

    Fixando erro function gerarNumeroAleatorio

--- test/utils.test.js
+++ test/utils.test.js
@@ -2,22 +2,22 @@ const { gerarNumeroAleatorio, acharCaracter } = require('../lib/utils');
 
 describe('Utils', () => {
     test('O início do intervalo é um valor negativo', () => {
-        const res = GerarNumeroAleatorio(-2, 5);
+        const res = gerarNumeroAleatorio(-2, 5);
         expect(res).toBe(-1);
     });
 
     test('O fim do intervalo é um valor negativo', () => {
-        const res = GerarNumeroAleatorio(2, -3);
+        const res = gerarNumeroAleatorio(2, -3);
         expect(res).toBe(-1);
     });
 
     test('O início do intervalo é igual ao fim do intervalo', () => {
-        const res = GerarNumeroAleatorio(6, 6);
+        const res = gerarNumeroAleatorio(6, 6);
         expect(res).toBe(-1);
     });
 
     test('Um intervalo válido [200, 3000] é fornecido', () => {
-        const res = GerarNumeroAleatorio(200, 3000);
+        const res = gerarNumeroAleatorio(200, 3000);
         expect(res > 200 && res < 3000);
     });
 
