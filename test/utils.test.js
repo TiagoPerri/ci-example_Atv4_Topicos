514f69c35f5ae32160b7b6a3bccb51214011eb87
TiagoPerri <tiagocnsr@gmail.com>
2021-10-16 01:15:09 -0300

    Refatorando, removendo bugs e adicionando testes para a function GerarNumeroAleatorio

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,11 +1,24 @@
 const { gerarNumeroAleatorio, acharCaracter } = require('../lib/utils');
 
 describe('Utils', () => {
-    describe('gerarNumeroAleatorio', () => {
-        test('fim nao pode ser negativo', () => {
-            expect(gerarNumeroAleatorio(20, -5))
-                .toBe(-10);
-        });
+    test('O início do intervalo é um valor negativo', () => {
+        const res = GerarNumeroAleatorio(-2, 5);
+        expect(res).toBe(-1);
+    });
+
+    test('O fim do intervalo é um valor negativo', () => {
+        const res = GerarNumeroAleatorio(2, -3);
+        expect(res).toBe(-1);
+    });
+
+    test('O início do intervalo é igual ao fim do intervalo', () => {
+        const res = GerarNumeroAleatorio(6, 6);
+        expect(res).toBe(-1);
+    });
+
+    test('Um intervalo válido [200, 3000] é fornecido', () => {
+        const res = GerarNumeroAleatorio(200, 3000);
+        expect(res > 200 && res < 3000);
     });
 
     describe('acharCaracter', () => {
